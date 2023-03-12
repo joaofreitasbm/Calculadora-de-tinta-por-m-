413d072db0594579dff922853fee8e48400222c5
João Freitas <joaofreitas0450@gmail.com>
2023-03-12 19:51:53 -0300

    correções infimas e remoção da biblioteca Decimal

--- calc.py
+++ calc.py
@@ -1,4 +1,3 @@
-from decimal import *
 import re
 import numpy as np
 
@@ -44,27 +43,27 @@ while True:
         # resultado 
         print('\nA tinta que você escolheu cobre', round(cobertura * tamLata), 'm² por lata.')
         print('Sua parede tem', areaParede, 'm²')
-        print('Você precisará de', round((lataTinta) + 0.49), 'lata (s) para cobrir a area de', areaParede, 'm²\n')        
+        print('Você precisará de', round((lataTinta) + 0.49), 'lata(s) para cobrir a area de', areaParede, 'm²\n')        
 
         # parametros piso
     elif modo == '2':
         print('Por gentileza, insira os dados solicitados abaixo.')
         nomeSala.append(input('Nome da sala: '))
-        largPiso = Decimal(input('Largura do piso em metros: '))
-        compPiso = Decimal(input('Comprimento do piso em metros: '))
+        largPiso = float(input('Largura do piso em metros: '))
+        compPiso = float(input('Comprimento do piso em metros: '))
         tamPeça = (input('Tamanho da peça de cerâmica (ex. 60x60, 120x120, etc): '))
         ceramSala.append(input('Nome da cerâmica: '))
         peça = re.split('[Xx]', tamPeça)
-        areaPeça = (Decimal(peça[0]) * Decimal(peça[1]))/10000
-        qntCaixa = Decimal(input('Quantas peças vem por caixa. '))
-        areaPiso = Decimal(largPiso) * Decimal(compPiso)
+        areaPeça = (float(peça[0]) * float(peça[1]))/10000
+        qntCaixa = float(input('Quantas peças vem por caixa. '))
+        areaPiso = float(largPiso) * float(compPiso)
         areaCaixa = round(float(areaPeça) * float(qntCaixa), 2)
         qntSala.append(round(float(areaPiso) / float(areaCaixa) + 0.49))
 
         # resultado 
         print('\nA cerâmica que você escolheu cobre', areaCaixa, 'm² por caixa.')
         print('Seu piso tem', areaPiso, 'm²')
-        print('Você precisará de', round(float(areaPiso) / float(areaCaixa) + 0.49), 'caixas (s) para cobrir a area de', areaPiso, 'm²\n')
+        print('Você precisará de', round(float(areaPiso) / float(areaCaixa) + 0.49), 'caixa(s) para cobrir a area de', areaPiso, 'm²\n')
     
     elif modo == '3': 
         resultParede = np.transpose(listaParede)
